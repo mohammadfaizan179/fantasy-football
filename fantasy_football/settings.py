800cf9e232fef3486c1d4066237210a908987807
Muhammad Faizan <faizan.meo179@gmail.com>
2026-08-23 22:52:04 +0500

    chore: undockerized the project

--- fantasy_football/settings.py
+++ fantasy_football/settings.py
@@ -32,7 +32,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    env('EC2_PUBLIC_IP')
+]
 
 
 # Application definition
